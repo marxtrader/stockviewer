b374956fec652da20de50ce8b8b9d5fcef2d74e3
Marcel Mukundi <marcelmukundi1@gmail.com>
2021-04-05 13:15:17 -0700

    Small optimizations for data-storing tests

--- api/functions/getTicks.mjs
+++ api/functions/getTicks.mjs
@@ -2,7 +2,7 @@
 const key = "TGH02yWO2sxsCHI_nwzD4Y7hlCJ0gXz02u6GWy"
 import * as PolygonUtils from '../old stock-viewer files/PolygonUtils.mjs'
 
-export async function getTicks(ticker, date, invalidConditions = [15, 16, 38, 17], limit = 1000) {
+export async function getTicks(ticker, date, invalidConditions = [15, 16, 38, 17], limit = 50000) {
     // Initialization of all the values we want to store
     let oddlotVolume = 0; let oddlotCount = 0; let volume = 0; let count = 0; let blockCount = 0; let blockVolume = 0;
 
@@ -17,6 +17,7 @@ export async function getTicks(ticker, date, invalidConditions = [15, 16, 38, 17
 
     do {
         data = await PolygonUtils.getTicksPage({ timestamp: t, limit, date, ticker })
+        console.log(data.results_count,t)
         if (data.results.length == 0) break;
 
         t = data.results[data.results_count - 1].t
@@ -41,7 +42,7 @@ export async function getTicks(ticker, date, invalidConditions = [15, 16, 38, 17
             }
         }
     }
-    while (data.result_count == limit);
+    while (data.results_count == limit);
     // if the result set is equal at the limit there are more records, so check for more.
 
     // Once the result count is less than limit, we must have gotten them all
@@ -56,8 +57,12 @@ export async function getTicks(ticker, date, invalidConditions = [15, 16, 38, 17
         blockCount,
         blockVolume
     }
-    // console.log(ticker,date,tickSummary)
+    // console.log(count)
+    console.log(ticker,date,tickSummary)
     return tickSummary
 }
 
+getTicks("AMC", "2021-01-29").then((data) => { console.log(data) })
+// getTicks("AMC", "2021-01-29").then((data) => { console.log(data) })
+// getTicks("AMC", "2021-01-28").then((data) => { console.log(data) })
 //getTicks("PTEN", "2021-03-01").then((data) => { console.log(data) })
